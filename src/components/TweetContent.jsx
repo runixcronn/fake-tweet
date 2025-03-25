eb46ecd4ed968fc5984e2ffc0928c71ced1e55ad
runixcronn <cemalettinreha92@gmail.com>
2025-03-25 15:34:35 +0300

    new push

--- src/components/TweetContent.jsx
+++ src/components/TweetContent.jsx
@@ -32,25 +32,25 @@ const ImagesContainer = ({ image }) => {
       );
     case 2:
       return (
-        <div className="mt-2 mb-2 w-full h-80 rounded-2xl border border-gray-300 overflow-hidden">
+        <div className="mt-2 mb-2 w-full h-80 rounded-2xl border border-gray-300 flex">
           <OneImageColumn side="left" image={image[0]} />
-          <div className="float-left w-0.5 h-full" />
+          <div className="w-0.5 h-full bg-gray-300" />
           <OneImageColumn side="right" image={image[1]} />
         </div>
       );
     case 3:
       return (
-        <div className="mt-2 mb-2 w-full h-80 rounded-2xl border border-gray-300 overflow-hidden">
+        <div className="mt-2 mb-2 w-full h-80 rounded-2xl border border-gray-300 flex">
           <OneImageColumn side="left" image={image[0]} />
-          <div className="float-left w-0.5 h-full" />
+          <div className="w-0.5 h-full bg-gray-300" />
           <TwoImagesColumn side="right" image1={image[1]} image2={image[2]} />
         </div>
       );
     case 4:
       return (
-        <div className="mt-2 mb-2 w-full h-80 rounded-2xl border border-gray-300 overflow-hidden">
+        <div className="mt-2 mb-2 w-full h-80 rounded-2xl border border-gray-300 flex">
           <TwoImagesColumn side="left" image1={image[0]} image2={image[1]} />
-          <div className="float-left w-0.5 h-full" />
+          <div className="w-0.5 h-full bg-gray-300" />
           <TwoImagesColumn side="right" image1={image[2]} image2={image[3]} />
         </div>
       );
@@ -61,15 +61,14 @@ const ImagesContainer = ({ image }) => {
 
 const OneImageColumn = ({ side, image }) => {
   side = side === "right" || side === "left" ? side : "left";
-  const borderSide = side.charAt(0);
 
   return (
     <div
-      className={`float-left w-1/2 h-full overflow-hidden ${
+      className={`w-1/2 h-full overflow-hidden ${
         side === "left" ? "rounded-l-2xl" : "rounded-r-2xl"
       }`}
     >
-      <img src={image} alt="" className="h-full min-w-full object-cover" />
+      <img src={image} alt="" className="h-full w-full object-cover" />
     </div>
   );
 };
@@ -78,7 +77,7 @@ const TwoImagesColumn = ({ side, image1, image2 }) => {
   side = side === "right" || side === "left" ? side : "left";
 
   return (
-    <div className={`float-left w-1/2 h-full`}>
+    <div className="w-1/2 h-full flex flex-col">
       <div
         className={`h-1/2 overflow-hidden ${
           side === "left" ? "rounded-tl-2xl" : "rounded-tr-2xl"
@@ -86,7 +85,7 @@ const TwoImagesColumn = ({ side, image1, image2 }) => {
       >
         <img src={image1} alt="" className="w-full h-full object-cover" />
       </div>
-      <div className="h-0.5 w-full" />
+      <div className="h-0.5 w-full bg-gray-300" />
       <div
         className={`h-1/2 overflow-hidden ${
           side === "left" ? "rounded-bl-2xl" : "rounded-br-2xl"
